8d7d68c9a725423b050c32a310939ea6e39f5ab1
irwn93 <irwn1993@gmail.com>
2018-04-21 18:31:36 -0500

    version inicial

---
+++ funcion.py
@@ -0,0 +1,11 @@
+import oper_avanced 
+
+def  sum_basic():
+    x = int(input('primer numero: '))
+    y = int (input('segundo numero: '))
+
+    s = x + y
+
+    print('el resultado de la suma es: ',s)
+
+oper_avanced.suma_avanced()
---
+++ oper_avanced.py
@@ -0,0 +1,9 @@
+def suma_avanced():
+
+    x = int(input('1er numero: '))
+    y = int(input('2do numero: '))
+    z = int(input('3er numero: '))
+
+    s = x + (y + z) + x
+
+    print('el resultado de la suma es: ', s)
