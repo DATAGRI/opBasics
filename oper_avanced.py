ffac83c698f0dd43e5b82a9a09c49275759d168a
irwn93 <irwn1993@gmail.com>
2018-04-21 18:43:44 -0500

    funcion resta avanzada

--- oper_avanced.py
+++ oper_avanced.py
@@ -7,3 +7,12 @@ def suma_avanced():
     s = x + (y + z) + x
 
     print('el resultado de la suma es: ', s)
+
+def resta_avanced():
+    x = int(input('1er numero: '))
+    y = int(input('2do numero: '))
+    z = int(input('3er numero: '))
+
+    r = x + (y + z) + x
+
+    print('el resultado de la suma es: ', r)
